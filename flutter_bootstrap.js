a61bd7492906ff47a6b8378e4b8e68af5ef68364
A7mdlbanna <A7mdlbanna@users.noreply.github.com>
2026-06-03 18:26:32 +0000

    deploy: ce25d7a68513982ac38b0a32ed5862eabf53e2c3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "722594820" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "465162063" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
